9568d5e04d3a79044093f63ea04c43a76bad6b9e
aejinkim <your_email@youremail.com>
2017-01-20 15:16:36 +0900

    add directory week-01-python

---
+++ week-01-python/03_data-type-2.py
@@ -0,0 +1,27 @@
+# 목록 list, tuple
+# 사전 dict - dictionary
+# 집합 set
+
+# list []
+print("list")
+# print([1, 2, 3])
+list_a = [1, 2, 3]
+print(list_a)
+# print(type([1, 2, 3]))
+# index는 0부터 시작합니다.
+print(list_a[0])
+
+#slice --> 마지막 인덱스는 포함하지 않는다. (인덱스 0부터 1까지)
+print(list_a[0:2])
+
+list_a.append(4)
+print(list_a)
+
+
+
+
+
+# tuple ()
+print("tuple")
+print((1, 2, 3))
+# print((1, 2, 3))
